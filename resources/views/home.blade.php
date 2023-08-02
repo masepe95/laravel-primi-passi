b48612278441712145145747968b96d1e241d62c
masepe95 <marcosepe10@gmail.com>
2023-08-02 20:04:38 +0200

    hello world printed

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel by Sepe</title>
+
+    <!-- Fonts -->
+
+
+    <!-- Styles -->
+    <style>
+
+    </style>
+</head>
+
+<body>
+    <h1>Hello World!</h1>
+
+</body>
+</body>
+
+</html>
